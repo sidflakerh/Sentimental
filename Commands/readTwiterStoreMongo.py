c6d031c66df14fd96f448d7b1cde30b8f8ca5cee
Praful Sharma <praful.sliet@gmail.com>
2020-01-13 14:44:52 +0530

    Update readTwiterStoreMongo.py

--- Commands/readTwiterStoreMongo.py
+++ Commands/readTwiterStoreMongo.py
@@ -7,10 +7,10 @@ from tweepy.streaming import StreamListener
 from pymongo import MongoClient
 
 
-consumer_key="Zf6OKZ1CKJ8asXOkHv2nANaQy"
-consumer_secret="ZyIt86TIm3o8u2vl4w7hTUffIRUKlQKUGXRX9jxmwB7osZSu4j"
-access_token="722762309857185793-VRe4FGILkon9wxVZgFt45jmjrvn9CVu"
-access_secret="RaBWS177JorNbWXSnqJIT8xJCvcnVXvJGACWIhaB5Yv4q"
+consumer_key=""
+consumer_secret=""
+access_token=""
+access_secret=""
 
 auth = OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_secret)
