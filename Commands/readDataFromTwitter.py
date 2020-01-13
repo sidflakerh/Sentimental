b626a5c7cbbbec63b9539be55bf27b326ae3b3d6
Praful Sharma <praful.sliet@gmail.com>
2020-01-13 14:44:09 +0530

    Update readDataFromTwitter.py

--- Commands/readDataFromTwitter.py
+++ Commands/readDataFromTwitter.py
@@ -4,10 +4,10 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy.streaming import StreamListener
 
-consumer_key="Zf6OKZ1CKJ8asXOkHv2nANaQy"
-consumer_secret="ZyIt86TIm3o8u2vl4w7hTUffIRUKlQKUGXRX9jxmwB7osZSu4j"
-access_token="722762309857185793-VRe4FGILkon9wxVZgFt45jmjrvn9CVu"
-access_secret="RaBWS177JorNbWXSnqJIT8xJCvcnVXvJGACWIhaB5Yv4q"
+consumer_key=""
+consumer_secret=""
+access_token=""
+access_secret=""
 
 auth = OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_secret)
